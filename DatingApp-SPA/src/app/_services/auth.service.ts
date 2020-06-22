8a885e300d82a427000f2261af05e533df76baf0
Leandro Alves Santos <lealsto82@gmail.com>
2020-06-22 08:54:38 -0300

    get the decoded token | set name on welcome area

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -10,16 +10,18 @@ export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
-        const user = response;
+        const user = response; 
 
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     );
@@ -32,6 +34,11 @@ export class AuthService {
   loggedIn() {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
+  }
+
+  setDecodedToken() {
+    const token = localStorage.getItem('token');
+    this.decodedToken = this.jwtHelper.decodeToken(token);
   }
 
 }
