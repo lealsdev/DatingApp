1736c481bcd3fd6103c9a191319b46c5b9d80789
Leandro Alves Santos <lealsto82@gmail.com>
2020-06-27 08:42:19 -0300

    User service added

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  baseurl = environment.apiUrl;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('token')
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.baseurl + 'users', this.httpOptions);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(this.baseurl + 'users/' + id, this.httpOptions);
+  }
+
+}
