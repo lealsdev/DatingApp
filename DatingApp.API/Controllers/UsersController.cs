b900adeba0e9ab24acd1981153ed3a9a8918cbfe
Leandro Alves Santos <lealsto82@gmail.com>
2020-07-02 19:11:07 -0300

    Fixing the getUsers method attributes

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -22,7 +22,7 @@ namespace DatingApp.API.Controllers
             this._repository = repository;
         }
 
-        [HttpGet("{id}", Name="GetUser")]
+        [HttpGet()]
         public async Task<IActionResult> GetUsers()
         {
             var users = await _repository.GetUsers();
@@ -32,7 +32,7 @@ namespace DatingApp.API.Controllers
             return Ok(usersToReturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repository.GetUser(id);
