30fc3e2a90e19a3e7fcce79d04aa7786d247d70e
Leandro Alves Santos <lealsto82@gmail.com>
2020-07-16 15:26:05 -0300

    Filtering members list by likers and likees API

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -31,5 +31,9 @@ namespace DatingApp.API.Helpers
 
         public string OrderBy { get; set; }
 
+        public bool Likees { get; set; } = false;
+
+        public bool Likers { get; set; } = false;
+
     }
 }
